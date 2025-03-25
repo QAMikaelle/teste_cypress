54cd931b2ae3a2a5e56a196ba29eb40ff05f8b53
QAMikaelle <mikaellequalidade@gmail.com>
2025-03-25 11:00:44 -0300

    adiciona configuração do Playwright e novos testes de integração

---
+++ cypress/e2e/trilhas.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe("Botão - nova trilha", () => {
+    beforeEach(() => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+          })
+             // Acesse a URL e clique no botão de idioma
+        cy.visit("https://www.hml.lector.live/testesautomatizados/subscribe/login");
+        
+        // Clica no botão de idioma
+        cy.get('.language-selection2 > :nth-child(1) > .btn').click();
+
+        // Realize o login
+        cy.get('#login_username').type("qualidade@lectortec.com.br");
+        cy.get('#login_password').type("123");
+        cy.get('#btn-entrar').click();
+    });
+
+    it("Clicar no botão de Trilhas", () => {
+        cy.wait(3000);
+        cy.get('[title="Trilhas"] > .sideitem').click();
+        
+        // Espera um pouco após clicar em Trilhas
+        cy.wait(1000);
+        
+        // Usa first() para garantir que apenas um botão seja clicado
+        cy.get('.title-bar > .icon-add')
+    });
+});
