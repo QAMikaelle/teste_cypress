65fdc6a3917bee3694662dde8dc805305b487394
QAMikaelle <mikaellequalidade@gmail.com>
2025-05-22 17:32:58 -0300

    att

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,34 +13,26 @@ describe("login", () => {
         cy.get('.message').should('have.text', 'Usuário ou senha inválidos'); 
     });
 
-
-    /*
-
       //cenarios
     it("Senha inválida", () => {
-        cy.visit('https://www.hml.lector.live/testesautomatizados/subscribe/login');
-        cy.get('#login_username').type('qualidade@lectortec.com.br');
-        cy.get('#login_password').type('000');
-        cy.get('#btn-entrar').contains("Entrar").click();
+        cy.visit('https://www.hml.lector.live/universolector/showcase/340');
+        cy.contains('button', 'Entrar').click();
+        cy.get('[style="z-index: 26;"] > :nth-child(1) > :nth-child(1) > .popup > :nth-child(1) > .ng-pristine').type('qualidade@lectortec.com.br');
+        cy.get('#login_password_navbar').type('000');
+        cy.get('.popup').contains('button', 'Entrar').click(); //filtrei pelo botão entrar que tinha no popup
         cy.get('.message').should('have.text', 'Usuário ou senha inválidos'); 
 
     });
-    
+
       //cenarios
     it("login com sucesso", () => {
       //DADO
       // abrir a aplicação
-        cy.visit("https://www.hml.lector.live/testesautomatizados/subscribe/login");
-
-      //preenche e-mail e senha
-        cy.get('#login_username').type("qualidade@lectortec.com.br");
-        cy.get('#login_password').type("123");
-
-      //clica em entrar
-        cy.get('#btn-entrar').contains("Entrar").click();
-
-    //ENTAO
-    //entra
-  });*/
+        cy.visit("https://www.hml.lector.live/universolector/showcase/340");
+        cy.contains('button', 'Entrar').click();
+        cy.get('[style="z-index: 26;"] > :nth-child(1) > :nth-child(1) > .popup > :nth-child(1) > .ng-pristine').type("qualidade@lectortec.com.br");
+        cy.get('#login_password_navbar').type("123");
+        cy.get('.popup').contains('button', 'Entrar').click();
+    });
 
 });
