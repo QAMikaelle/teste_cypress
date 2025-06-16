427a145c55c90c8810562309a02958387e7599d8
QAMikaelle <mikaellequalidade@gmail.com>
2025-06-16 11:49:18 -0300

    att

--- cypress/e2e/vitrines.cy.js
+++ cypress/e2e/vitrines.cy.js
@@ -1,30 +1,16 @@
 /// <reference types="cypress" />
 
-describe("Vitrines - Criar Nova Categoria", () => {
+describe("Teste - Vitrine", () => {
     beforeEach(() => {
-        Cypress.on('uncaught:exception', (err, runnable) => {
-            return false
-          })
-        // Acesse a URL e clique no botão de idioma
-        cy.visit("https://www.hml.lector.live/testesautomatizados/subscribe/login");
-        
-        // Clica no botão de idioma
-        cy.get('.language-selection2 > :nth-child(1) > .btn').click();
-        
-        // Realize o login
-        cy.get('#login_username').type("qualidade@lectortec.com.br");
-        cy.get('#login_password').type("123");
-        cy.get('#btn-entrar').click();
+
+        cy.visit("https://www.hml.lector.live/universolector/showcase/340");
+        cy.contains('button', 'Entrar').click();
+        cy.get('[style="z-index: 26;"] > :nth-child(1) > :nth-child(1) > .popup > :nth-child(1) > .ng-pristine').type("qualidade@lectortec.com.br");
+        cy.get('#login_password_navbar').type("123");
+        cy.get('.popup').contains('button', 'Entrar').click();
     });
 
     it("Clicar no botão de vitrines", () => {
-        cy.wait(3000);
-        cy.get('[title="Vitrines"]').click();
         
-        // Espera um pouco após clicar em Vitrines
-        cy.wait(1000);
-        
-        // Usa first() para garantir que apenas um botão seja clicado
-        cy.get('.title-bar').click();
     });
 });
